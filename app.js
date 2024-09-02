f8080f891e436e594103fee8df30df2a5279e944
valentin-orrit <vorrit@gmail.com>
2024-09-02 18:35:24 +0200

    created config file to connect to mongoDB

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
 
+const connectDB = require('./server/config/db')
+
 const app = express()
-const PORT = process.env.PORT || 3000  
+const PORT = process.env.PORT || 3000
+
+// Connect to DB
+connectDB()
 
 app.use(express.static('public'))
 
