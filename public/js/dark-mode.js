88d1695b62cfe83088122c145e4e739c889558e3
valentin-orrit <vorrit@gmail.com>
2025-01-27 15:16:56 +0100

    fixed dark mode

--- public/js/dark-mode.js
+++ public/js/dark-mode.js
@@ -1,15 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.getElementById('darkModeToggle')
     const body = document.body
+    const profilePicture = document.getElementById('hero-profile-picture')
 
     function setDarkMode(isDark) {
         body.classList.toggle('dark-mode', isDark)
         localStorage.setItem('darkMode', isDark ? 'enabled' : null)
+
+        if (isDark) {
+            profilePicture.style.filter = 'none'
+        } else {
+            profilePicture.style.filter = 'invert(1)'
+        }
     }
 
-    if (localStorage.getItem('darkMode') === 'enabled') {
-        setDarkMode(true)
-    }
+    const isDarkModeEnabled = localStorage.getItem('darkMode') === 'enabled'
+    setDarkMode(isDarkModeEnabled)
 
     darkModeToggle.addEventListener('click', () => {
         setDarkMode(!body.classList.contains('dark-mode'))
